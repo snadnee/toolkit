b9caa7319fcc57de06ddd3228da298d7e8431fbe
Martin <machacek@snadnee.com>
2024-10-22 09:00:54 +0200

    Add base command for initializing new project with optional features

---
+++ src/Commands/InitializeProjectCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Snadnee\Toolkit\Commands;
+
+use Illuminate\Console\Command;
+use function Laravel\Prompts\multiselect;
+
+class InitializeProjectCommand extends Command
+{
+    protected $signature = 'toolkit:init';
+
+    protected $description = 'Select which toolkit features you want to use in this project';
+
+    public function handle(): int
+    {
+        multiselect(
+            label: 'Select which toolkit features you want to use in this project',
+            options: [
+                'githooks' => 'Pre-commit hooks',
+                'workflows' => 'Github workflows',
+                'be_translations' => 'Backend translations',
+                'fe_translations' => 'Frontend translations',
+            ]
+        );
+
+        return 0;
+    }
+}
